6d71e8c5d9223dbf2f719be77fa229cf0fc284e9
hirohira <hiramoto@madoc.co.jp>
2025-05-08 18:04:33 +0900

    Update autoload.php

--- autoload.php
+++ autoload.php
@@ -4,12 +4,10 @@ spl_autoload_register(function ($class) {
     $prefix  = 'App\\';
     $baseDir = __DIR__ . '/app/';
 
-    // 名前空間が App\ で始まらない場合はスキップ
     if (strncmp($prefix, $class, strlen($prefix)) !== 0) {
         return;
     }
 
-    // 相対クラス名を取得してファイルパスに変換
     $relativeClass = substr($class, strlen($prefix));
     $file = $baseDir . str_replace('\\', '/', $relativeClass) . '.php';
 
